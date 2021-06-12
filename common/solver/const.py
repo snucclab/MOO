ba1e92d821d6f0bd53599bbe900dd15edabe670b
jisuAn <ajs7270@naver.com>
2021-06-12 17:49:39 +0900

    ADD MOD operator

--- common/solver/const.py
+++ common/solver/const.py
@@ -66,63 +66,65 @@ OPR_VALUES = [
      PRECEDENCE: 3},
     # 8. DIV(float,float)
     {NAME: OPR_DIV, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: (lambda *x: x[0] / x[1]),
+    # 9. MOD(int, int)
+    {NAME: OPR_MOD, ARITY: 1, COMMUTATIVE: False, ISVOID: False, CONVERT: (lambda *x: x[0] / x[1]),
      PRECEDENCE: 3},
-    # 9. POW(float,float)
+    # 10. POW(float,float)
     {NAME: OPR_POW, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: (lambda *x: x[0] ** x[1]),
      PRECEDENCE: 4},
-    # 10. PRINT(string_or_int_or_float)
+    # 11. PRINT(string_or_int_or_float)
     {NAME: OPR_PRINT, ARITY: 1, COMMUTATIVE: True, ISVOID: True, CONVERT: (lambda *x: print(x)), PRECEDENCE: None},
-    # 11. SUM(List)
+    # 12. SUM(List)
     {NAME: OPR_SUM, ARITY: 1, COMMUTATIVE: False, ISVOID: False, CONVERT: (lambda *x: sum(range(x[0],x[1],x[2]))), PRECEDENCE: None},
-    # 12. LIST(): create list
+    # 13. LIST(): create list
     {NAME: OPR_LIST, ARITY: 0, COMMUTATIVE: False, ISVOID: False, CONVERT: (lambda *x: list(*x)), PRECEDENCE: None},
-    # 13. APPEND(List,Any)
+    # 14. APPEND(List,Any)
     {NAME: OPR_APPEND, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 14. COMB(int,int) 
+    # 15. COMB(int,int) 
     {NAME: OPR_COMB, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 15. PERM(int,int)
+    # 16. PERM(int,int)
     {NAME: OPR_PERM, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 16. MIN(List)
+    # 17. MIN(List)
     {NAME: OPR_MIN, ARITY: 1, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 17. MAX(List)
+    # 18. MAX(List)
     {NAME: OPR_MAX, ARITY: 1, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 18. RANGE(start: int, end: int, step: int)
+    # 19. RANGE(start: int, end: int, step: int)
     {NAME: OPR_RANGE, ARITY: 3, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 19. LCM(List)
+    # 20. LCM(List)
     {NAME: OPR_LCM, ARITY: 1, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 20. GCD(List)
+    # 21. GCD(List)
     {NAME: OPR_GCD, ARITY: 1, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 21. COUNT_MULTI(List,List): function for the 13th question
+    # 22. COUNT_MULTI(List,List): function for the 13th question
     {NAME: OPR_COUNT_MULTI, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 22. DIGIT(int, digit: int)
+    # 23. DIGIT(int, digit: int)
     {NAME: OPR_DIGIT, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 23. TO_INT(float)
+    # 24. TO_INT(float)
     {NAME: OPR_TO_INT, ARITY: 1, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 24. CALL_SYMPY(List)
+    # 25. CALL_SYMPY(List)
     {NAME: OPR_CALL_SYMPY, ARITY: 1, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 25. REVERSE_DIGIT(int)
+    # 26. REVERSE_DIGIT(int)
     {NAME: OPR_REVERSE_DIGIT, ARITY: 1, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 26. SEQ_TERM(List, int)
+    # 27. SEQ_TERM(List, int)
     {NAME: OPR_SEQ_TERM, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 27. REP_SEQ_TERM(List, int)
+    # 28. REP_SEQ_TERM(List, int)
     {NAME: OPR_REP_SEQ_TERM, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 28. MAKE_PAIR(str, str_or_int)
+    # 29. MAKE_PAIR(str, str_or_int)
     {NAME: OPR_MAKE_PAIR, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 29. COUNT(List) 
+    # 30. COUNT(List) 
     {NAME: OPR_COUNT, ARITY: 1, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 30. LT(List, int)
+    # 31. LT(List, int)
     {NAME: OPR_LT, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 31. LE(List, int)
+    # 32. LE(List, int)
     {NAME: OPR_LE, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 32. GT(List, int)
+    # 33. GT(List, int)
     {NAME: OPR_GT, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 33. GE(List, int)
+    # 34. GE(List, int)
     {NAME: OPR_GE, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 34. LIST_CONCAT(ls1: List[Any], ls2: List[Any]) -> List[Any]
+    # 35. LIST_CONCAT(ls1: List[Any], ls2: List[Any]) -> List[Any]
     {NAME: OPR_LIST_CONCAT, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 35. LIST_INDEX(ls: List[Union[int,float,str]],item: Union[str, int, float]) -> int
+    # 36. LIST_INDEX(ls: List[Union[int,float,str]],item: Union[str, int, float]) -> int
     {NAME: OPR_LIST_INDEX, ARITY: 2, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None},
-    # 36. LIST_REPLACE(ls: List[Union[int,float,str]], n: int, item: Union[int,float,str]) -> List[Union[int,float,str]]
+    # 37. LIST_REPLACE(ls: List[Union[int,float,str]], n: int, item: Union[int,float,str]) -> List[Union[int,float,str]]
     {NAME: OPR_LIST_REPLACE, ARITY: 3, COMMUTATIVE: False, ISVOID: False, CONVERT: None, PRECEDENCE: None}
 ]
 
