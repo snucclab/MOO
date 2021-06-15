36b789d260c16c94162671d9ec0364c869860126
nearbydelta <cd4209@gmail.com>
2021-06-16 00:52:28 +0900

    Add non-parallel code for debugging

--- train_model_debug.py
+++ train_model_debug.py
@@ -1,17 +1,9 @@
 import logging
 from argparse import ArgumentParser
-from os import cpu_count
 from sys import argv
 
-from ray import tune, init, shutdown
-from ray.tune.trial import Trial
-from ray.tune.utils.util import is_nan_or_inf
-from torch.cuda import device_count
-
 from common.model.const import *
-from common.sys.const import EVALUATE_WEIGHT_PATH, EVALUATE_TOKENIZER_PATH
 from learner import *
-from shutil import copy
 
 CPU_FRACTION = 1.0
 GPU_FRACTION = 0.5
@@ -28,13 +20,12 @@ def read_arguments():
     env.add_argument('--beam', '-beam', type=int, default=3)
 
     env.add_argument('--max-iter', '-iter', type=int, default=100)
-    env.add_argument('--stop-conditions', '-stop', type=str, nargs='*', default=[])
 
     model = parser.add_argument_group('Model')
     model.add_argument('--encoder', '-enc', type=str, default=DEF_ENCODER)
     model.add_argument('--decoder-hidden', '-decH', type=int, default=0)
     model.add_argument('--decoder-intermediate', '-decI', type=int, default=0)
-    model.add_argument('--decoder-layer', '-decL', type=int, default=[6], nargs='+')
+    model.add_argument('--decoder-layer', '-decL', type=int, default=6)
     model.add_argument('--decoder-head', '-decA', type=int, default=0)
 
     log = parser.add_argument_group('Logger setup')
@@ -45,12 +36,12 @@ def read_arguments():
     work.add_argument('--num-gpu', '-gpu', type=float, default=GPU_FRACTION)
 
     setup = parser.add_argument_group('Optimization setup')
-    setup.add_argument('--opt-lr', '-lr', type=float, default=[0.00176], nargs='+')
+    setup.add_argument('--opt-lr', '-lr', type=float, default=0.00176)
     setup.add_argument('--opt-beta1', '-beta1', type=float, default=0.9)
     setup.add_argument('--opt-beta2', '-beta2', type=float, default=0.999)
     setup.add_argument('--opt-eps', '-eps', type=float, default=1E-8)
     setup.add_argument('--opt-grad-clip', '-clip', type=float, default=10.0)
-    setup.add_argument('--opt-warmup', '-warmup', type=float, default=[2], nargs='+')
+    setup.add_argument('--opt-warmup', '-warmup', type=float, default=2)
     setup.add_argument('--batch-size', '-bsz', type=int, default=4)
 
     return parser.parse_args()
@@ -87,7 +78,7 @@ def build_configuration(args):
             MDL_DECODER: {
                 MDL_D_HIDDEN: args.decoder_hidden,
                 MDL_D_INTER: args.decoder_intermediate,
-                MDL_D_LAYER: tune.grid_search(args.decoder_layer),
+                MDL_D_LAYER: args.decoder_layer,
                 MDL_D_HEAD: args.decoder_head
             }
         },
@@ -99,36 +90,23 @@ def build_configuration(args):
         KEY_GRAD_CLIP: args.opt_grad_clip,
         KEY_OPTIMIZER: {
             'type': 'lamb',
-            'lr': tune.grid_search(args.opt_lr),
+            'lr': args.opt_lr,
             'betas': (args.opt_beta1, args.opt_beta2),
             'eps': args.opt_eps,
             'debias': True
         },
         KEY_SCHEDULER: {
             'type': 'warmup-linear',
-            'num_warmup_epochs': tune.grid_search(args.opt_warmup),
+            'num_warmup_epochs': args.opt_warmup,
             'num_total_epochs': args.max_iter
         }
     }
-
-
-def build_stop_condition(args):
-    cond_dict = dict(training_iteration=args.max_iter)
-    for condition in args.stop_conditions:
-        key, value = condition.split('=')
-        cond_dict[key] = float(value)
-
-    return cond_dict
 
 
 def get_experiment_name(args):
     from datetime import datetime
     now = datetime.now().strftime('%m%d%H%M%S')
     return f'{args.name}_{now}'
-
-
-def trial_dirname_creator(trial: Trial) -> str:
-    return trial.trial_id
 
 
 if __name__ == '__main__':
@@ -147,65 +125,11 @@ if __name__ == '__main__':
 
     logger.info('========================= CMD ARGUMENT =============================')
     logger.info(' '.join(argv))
-    init(num_cpus=cpu_count(), num_gpus=device_count())
 
     experiment_name = get_experiment_name(args)
+    trainer = SupervisedTrainer(build_configuration(args))
 
-    stop_condition = build_stop_condition(args)
-    analysis = tune.run(SupervisedTrainer, name=experiment_name, stop=stop_condition,
-                        config=build_configuration(args), local_dir=args.log_path, checkpoint_at_end=True,
-                        checkpoint_freq=args.max_iter // 5, reuse_actors=True,
-                        trial_dirname_creator=trial_dirname_creator, raise_on_failed_trial=False,
-                        metric='dev_correct', mode='max')
+    for _ in range(args.max_iter):
+        trainer.step()
 
-    # Record trial information
-    logger.info('========================= DEV. RESULTS =============================')
-    logger.info('Hyperparameter search is finished!')
-    trials: List[Trial] = analysis.trials
-    best_scores = -1.0
-    best_configs = {}
-    best_trial = None
-
-    for trial in trials:
-        if trial.status != Trial.TERMINATED:
-            logger.info('\tTrial %10s (%-40s): FAILED', trial.trial_id, trial.experiment_tag)
-            continue
-
-        last_score = trial.last_result['dev_correct']
-        logger.info('\tTrial %10s (%-40s): Correct %.4f on dev. set', trial.trial_id, trial.experiment_tag, last_score)
-
-        if is_nan_or_inf(last_score):
-            continue
-
-        if best_scores < last_score:
-            best_scores = last_score
-            best_configs = trial.config
-            best_trial = trial
-
-    # Record the best configuration
-    logpath = Path(analysis.best_logdir).parent
-    logger.info('--------------------------------------------------------')
-    logger.info('Found best configuration (scored %.4f)', best_scores)
-    logger.info(repr(best_configs))
-    logger.info('--------------------------------------------------------')
-    with Path(logpath, 'best_config.pkl').open('wb') as fp:
-        pickle.dump(best_configs, fp)
-    with Path(logpath, 'best_config.yaml').open('w+t') as fp:
-        yaml_dump(best_configs, fp)
-
-    # Copy the best configuration to weights directory
-    weight_path = Path(EVALUATE_WEIGHT_PATH)
-    tokenizer_path = Path(EVALUATE_TOKENIZER_PATH)
-
-    if not weight_path.parent.exists():
-        weight_path.parent.mkdir(parents=True)
-    else:
-        if weight_path.exists():
-            weight_path.unlink()
-        if tokenizer_path.exists():
-            tokenizer_path.unlink()
-
-    copy(Path(best_trial.checkpoint, 'EPT.pt'), weight_path)
-    copy(Path(best_trial.checkpoint, 'tokenizer.pt'), tokenizer_path)
-
-    shutdown()
+    trainer.save_checkpoint('./runs/test-checkpoint')
